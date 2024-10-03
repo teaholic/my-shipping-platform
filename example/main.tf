34101c6719b06a0f06513ca843f931ddf66e058f
Chiara Barbagianni <chiara.barbagianni@agilelab.it>
2024-10-03 11:33:42 +0200

    Improving first quasi-test implementation

--- example/main.tf
+++ example/main.tf
@@ -1,5 +1,23 @@
+# Arrange / Given
+locals {
+  delivery_type = "eu-parcel-to-tuscany"
+  expected_item = "parcel"
+}
+
+# Act / When
 module "shipping-an-eu-parcel-to-tuscany" {
+
   source = "../"
 
-  delivery_type = "eu-parcel-to-tuscany"
+  delivery_type = local.delivery_type
+}
+
+# Assert / Then
+output "eu-parcel-to-tuscany_shipped_item" {
+  value = module.shipping-an-eu-parcel-to-tuscany.item
+
+  precondition {
+    condition = module.shipping-an-eu-parcel-to-tuscany.item == local.expected_item
+    error_message = "Error: ${local.delivery_type} should ship a ${local.expected_item}. Actual shipped item: ${module.shipping-an-eu-parcel-to-tuscany.item}"
+  }
 }
--- example/output.tf
+++
@@ -1,8 +0,0 @@
-output "eu-parcel-to-tuscany_shipped_item" {
-  value = module.shipping-an-eu-parcel-to-tuscany.item
-
-  precondition {
-    condition = module.shipping-an-eu-parcel-to-tuscany.item == "parcel"
-    error_message = "Error: eu-parcel-to-tuscany should ship a parcel. Actual shipped item: ${module.shipping-an-eu-parcel-to-tuscany.item}"
-  }
-}
